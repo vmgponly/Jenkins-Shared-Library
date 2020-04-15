aac3f361028a3e723ba6dd20df8455758c6e85d6
vmgponly <54068461+vmgponly@users.noreply.github.com>
2020-04-15 09:48:16 +0530

    Update JavaServiceBuildDeployment.groovy

--- vars/JavaServiceBuildDeployment.groovy
+++ vars/JavaServiceBuildDeployment.groovy
@@ -21,7 +21,12 @@ pipeline {
             script
              {    
                def subFunction = new javaServiceSubFunction()
-             }        
+             }
+          sh '''
+             echo ${config.service_name}
+             echo ${config.application}
+                 '''
+       
             
          }
       }
